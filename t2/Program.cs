8f626c7403b33cb18d03a591b1780f96cf3cdc88
Rishat <risatnurullin53@gmail.com>
2022-08-25 08:19:42 +0300

    task2

---
+++ t2/Program.cs
@@ -0,0 +1,17 @@
+﻿int SummaNumber(int m, int n, int count, int sum)
+{
+    sum = sum + count;
+    count++;
+    if (count <= n)
+    { return SummaNumber(m, n, count, sum); }
+    else
+    { return sum; }
+}
+
+Console.Write("Введите значения m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значения n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = m;
+int sum = SummaNumber(m, n, count, 0);
+Console.WriteLine("Сумма  = " + sum);
