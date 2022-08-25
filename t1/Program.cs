2d37e3ad88822826b6bf57685f201dc3b034249b
Rishat <risatnurullin53@gmail.com>
2022-08-25 08:19:01 +0300

    task1

---
+++ t1/Program.cs
@@ -0,0 +1,13 @@
+﻿//№64
+void PrintNumber(int n)
+{
+    if (n < 1)
+    {return;}
+    Console.Write(n + " ");
+    PrintNumber(n-1); 
+}
+
+Console.Write("Введите значение N ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumber(n);
+Console.WriteLine("");
